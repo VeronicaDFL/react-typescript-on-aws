aa8287a78329c5acd0f8a92d2552be138c64c6a1
Veronica De Felice <verodefelic@gmail.com>
2023-05-04 13:43:18 -0400

    FIRST repo,create and deploy aws

---
+++ src/components/Form.tsx
@@ -0,0 +1,34 @@
+import React, {FormEvent, useState} from "react"
+import { HelloProps } from "../types/formDataTypes"
+
+
+export const Form = () => {
+    const [formData, setFormData] = useState<HelloProps>({firstName:'', lastName:'',age: 0})
+
+    const handleFormSubmit =(e: FormEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+
+
+    }
+    
+    return (
+        <>
+        <form action="submit">
+            <input type="text" 
+            placeholder="First name" 
+            onChange={e => setFormData({...formData,firstName: e.target.value})}/>
+
+            <input type="text"  
+            placeholder= "Last name" 
+            onChange={e => setFormData({...formData,lastName: e.target.value})}/>
+
+            <input type="number"  
+            placeholder= "Age-optional" 
+            onChange={e => setFormData({...formData,age:Number(e.target.value)})}/>
+
+            <button type="submit" 
+            onClick = {(e) => handleFormSubmit(e)}></button>
+            </form>
+        </>
+    )
+}
---
+++ src/types/formDataTypes.ts
@@ -0,0 +1,7 @@
+
+
+ export interface HelloProps {
+    firstName: string
+    lastName: string
+    age?: number    ////? optional
+}
