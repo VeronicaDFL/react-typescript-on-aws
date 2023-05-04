4eb73cae003e46fb51795e3a9d39c2a40eb8839c
Veronica De Felice <verodefelic@gmail.com>
2023-05-04 16:08:56 -0400

    connecting to API

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,23 @@ import { Form }from './components/Form'
 
 
 function App() {
+
+  const [serverData, setServerData] = React.useState([])
+
+React.useEffect(() => {
+  fetch('http://3.136.108.180:4000/')
+  .then(res =>res.json())
+  .then(data => setServerData(data))
+  .catch(err => console.error(err))
+},[])
+
   return (
     <div className="App">
       <header className="App-header">
        <Hello firstName= "Veronica" lastName= "De Felice"/>
        <Form />
+       {/* @ts-ignore */}
+       {serverData && serverData.map(eachDog => <p>{eachDog.name} - {eachDog.age}</p>)}
       </header>
     </div>
   );
